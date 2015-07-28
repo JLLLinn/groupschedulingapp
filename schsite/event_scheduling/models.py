37c56b629141f2a9aa83facd7e1172f9c859e1df
Jiaxin Lin <craiglin1992@gmail.com>
2015-07-28 01:28:21 -0500

    add a bit models

--- schsite/event_scheduling/models.py
+++ schsite/event_scheduling/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Event(models.Model):
+    """
+    The event model that stores the organizer information,
+    """
+    # An Event will have an organizer, it can be blank or null, which means it has no organizer
+    organizer = models.ForeignKey(User
+                                  , blank=True
+                                  , null=True
+                                  , on_delete=models.SET_NULL)
+
+    # An Event will have a list of person that are attending, allowing null and blank initially, or whatever
+    attendees = models.ManyToManyField(User
+                                       , blank=True
+                                       , null=True)
+
+    #An event will also have a list of time,
+
+
+class User(models.Model):
+    pass
