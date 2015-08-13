19feab8156e021df5575d4725b774c0bf1b7f877
Jiaxin Lin <craiglin1992@gmail.com>
2015-08-13 02:25:20 -0500

    a bit

--- schsite/event_scheduling/views.py
+++ schsite/event_scheduling/views.py
@@ -65,15 +65,14 @@ def get_plan(request, event_hid):
 
 @ensure_csrf_cookie
 def set_precise_time_for_dates(request, event_hid):
+    #TODO
     event_primary_keys = hashids.decode(event_hid)
     if len(event_primary_keys) >= 1:
         event = get_object_or_404(Event, pk=event_primary_keys[0])
         context_obj = {
+            "TIME_TYPE_PRECISE_TIME_TIME": Timeslot.PRECISE_TIME_TIME,
             "event": event,
-            "event_hid": event_hid,
-            "MIN_NAME_LENGTH": MIN_NAME_LENGTH,
-            "MIN_CELL_WIDTH": MIN_CELL_WIDTH,
-
+            "event_hid": event_hid
         }
         return render(request, 'event_scheduling/set_precise_time_for_dates.html', context_obj)
     else:
@@ -106,6 +105,37 @@ def add_whole_day(request):
             'event_hashid': event_hashid,
             'eventusertimeslots_hashid': eventusertimeslots_hashid,
             'url': reverse("event_scheduling:fetch_plan", args=(event_hashid,))
+        }
+        return JsonResponse(response_obj)
+    else:
+        raise Http404("Oops, 这里啥都木有。。。")
+
+
+def add_dates_for_precise_time_event(request):
+    """
+    Create a precise time event, handles post
+    :param request:
+    :return:
+    """
+    #TODO
+    if request.method == 'POST' and request.is_ajax():
+        title = request.POST.get('event_title', False)
+        date_raw_post = request.POST.get('dates', False)
+        organizer_name = request.POST.get('organizer_name', False)
+        if not (title and date_raw_post and organizer_name):
+            raise Http404("Oops, 这里啥都木有。。。")
+        date_strs = date_raw_post.split(DATE_STR_SPLITTER)
+        dates = []
+        for date_str in date_strs:
+            dates.append(datetime.datetime.strptime(date_str, "%m/%d/%Y").date())
+        event_primary_key, eventusertimeslots_primary_key = init_whole_day_event(title, dates,
+                                                                                 organizer_name)
+        event_hashid = hashids.encode(event_primary_key)
+        eventusertimeslots_hashid = hashids.encode(eventusertimeslots_primary_key)
+        response_obj = {
+            'event_hashid': event_hashid,
+            'eventusertimeslots_hashid': eventusertimeslots_hashid,
+            'url': reverse("event_scheduling:set_precise_time_for_dates", args=(event_hashid,))
         }
         return JsonResponse(response_obj)
     else:
